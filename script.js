3532b192981c04cf6e62c9c3b49a57e8070b134a
asikoshhh <kemelbaev.aktau@gmail.com>
2026-04-21 23:20:26 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-// Данные фильмов
 const movies = [
     {
         id: 1,
         title: "Аватар: Путь воды",
         genre: "Фантастика",
-        poster: "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg"
+        poster: "https://avatars.mds.yandex.net/i?id=2a2c3a8e5b5a4c5e8a5e5e5a5c5e5a5e"
     },
     {
         id: 2,
         title: "Оппенгеймер",
         genre: "Драма",
-        poster: "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg"
+        poster: "https://avatars.mds.yandex.net/i?id=8f5e5a5e5a5e5a5e8a5e5e5a5c5e5a5e"
     },
     {
         id: 3,
         title: "Барби",
         genre: "Комедия",
-        poster: "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg"
+        poster: "https://avatars.mds.yandex.net/i?id=5a5e5a5e5a5e5a5e8a5e5e5a5c5e5a5e"
     },
     {
         id: 4,
         title: "Джон Уик 4",
         genre: "Боевик",
-        poster: "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg"
+        poster: "https://avatars.mds.yandex.net/i?id=2a2c3a8e5b5a4c5e8a5e5e5a5c5e5a5e"
     }
 ];
 
-// Показать фильмы на странице
 function displayMovies() {
     const grid = document.getElementById('movies-grid');
+    if (!grid) return;
+    
     grid.innerHTML = '';
     
     movies.forEach(movie => {
         const card = document.createElement('div');
         card.className = 'movie-card';
         card.innerHTML = `
-            <img class="movie-poster" src="${movie.poster}" alt="${movie.title}">
+            <img class="movie-poster" src="${movie.poster}" alt="${movie.title}" onerror="this.src='https://placehold.co/300x450/0f3460/e94560?text=${encodeURIComponent(movie.title)}'">
             <div class="movie-info">
                 <div class="movie-title">${movie.title}</div>
                 <div class="movie-genre">${movie.genre}</div>
@@ -45,5 +45,4 @@ function displayMovies() {
     });
 }
 
-// Запуск
 displayMovies();
